7413bfc515fa3db7de2b28106e56de23e19219d6
Victor Chan <victorchan654@gmail.com>
2023-09-04 03:17:49 -0400

    Added functions to connect to Kafka

--- youtube_watcher.py
+++ youtube_watcher.py
@@ -2,6 +2,10 @@ import logging
 from pprint import pformat
 import sys
 import json
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.serialization import StringSerializer
+from confluent_kafka.schema_registry.avro import AvroSerializer
+from confluent_kafka import SerializingProducer
 from config import config
 import requests
 
@@ -24,7 +28,7 @@ def fetch_videos_page(googleApiKey, videoId, pageToken=None):
                             params={
                                 "key": googleApiKey,
                                 "id": videoId,
-                                "part": "snippet",
+                                "part": "snippet,statistics",
                                 "pageToken":pageToken
                             })
     payload = json.loads(response.text)
@@ -32,19 +36,6 @@ def fetch_videos_page(googleApiKey, videoId, pageToken=None):
     # logging.debug("GOT %s", payload)
 
     return payload
-
-def fetch_videos(googleApiKey, playlistId, pageToken=None):
-    # Fetch one page
-    payload = fetch_videos_page(googleApiKey,playlistId, pageToken)
-
-    # Serve up items from that page
-    yield from payload["items"]
-
-    nextPageToken = payload.get("nextPageToken")
-
-    # If there's another page
-    if nextPageToken:
-        yield from fetch_videos(googleApiKey, playlistId, nextPageToken)
 
 def fetch_playlist_items(googleApiKey, playlistId, pageToken=None):
     # Fetch one page
@@ -59,6 +50,19 @@ def fetch_playlist_items(googleApiKey, playlistId, pageToken=None):
     if nextPageToken:
         yield from fetch_playlist_items(googleApiKey, playlistId, nextPageToken)
 
+def fetch_videos(googleApiKey, playlistId, pageToken=None):
+    # Fetch one page
+    payload = fetch_videos_page(googleApiKey,playlistId, pageToken)
+
+    # Serve up items from that page
+    yield from payload["items"]
+
+    nextPageToken = payload.get("nextPageToken")
+
+    # If there's another page
+    if nextPageToken:
+        yield from fetch_videos(googleApiKey, playlistId, nextPageToken)
+
 def summarizeVideo(video):
     return {
         "videoId": video["id"],
@@ -68,16 +72,46 @@ def summarizeVideo(video):
         "comments": video["statistics"].get("commentCount",0)
     }
 
+def onDelivery(err, record):
+    pass
+
 
 def main():
     logging.info("START")
+
+    schemaRegistryClient = SchemaRegistryClient(config["SCHEMA_REGISTRY"])
+    youtubeVideosValueSchema = schemaRegistryClient.get_latest_version("youtube_videos-value")
+
+    kafkaConfig = config["KAFKA"] | {
+        "key.serializer": StringSerializer(),
+        "value.serializer": AvroSerializer(
+            schemaRegistryClient,
+            youtubeVideosValueSchema.schema.schema_str,
+        )
+    }
+    producer = SerializingProducer(kafkaConfig)
+
     googleApiKey = config["GOOGLE_API_KEY"]
     playlistId = config["YOUTUBE_PLAYLIST_ID"]
 
     for videoItem in fetch_playlist_items(googleApiKey, playlistId):
         videoId = videoItem["contentDetails"]["videoId"]
         for video in fetch_videos(googleApiKey, videoId):
-            logging.info("GOT %s", pformat(video))
+            logging.info("GOT %s", pformat(summarizeVideo(video)))
+
+            producer.produce(
+                topic="youtube_videos",
+                key = videoId,
+                value = {
+                    "TITLE": video["snippet"]["title"],
+                    "VIEWS": int(video["statistics"].get("viewCount",0)),
+                    "LIKES": int(video["statistics"].get("likeCount",0)),
+                    "COMMENTS": int(video["statistics"].get("commentCount",0))
+                },
+                on_delivery = onDelivery
+            )
+    
+    producer.flush()
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
